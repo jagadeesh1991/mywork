d5b937ea0fcbb8b63f42fbafcde1dbd3580d8002
jagadeesh1991 <jagadeesh.aj@gmail.com>
2020-02-02 11:26:58 +0530

    Add files via upload

--- sta.py
+++ sta.py
@@ -10,6 +10,7 @@ from flask import Response
 from flask_cors import CORS
 from flask_api import FlaskAPI
 import os
+import datetime
 #import pwd
 APP = FlaskAPI(__name__)
 CORS(APP)
@@ -21,9 +22,9 @@ def get_stats_full():
         st = os.stat(filenamepath)
        # userinfo = pwd.getpwuid(st.ST_UID)
         
-        response_dict.update({'Last Access Time': st.st_atime})
-        response_dict.update({'Last Modification Time': st.st_mtime})
-        response_dict.update({'Last Change Time': st.st_ctime})
+        response_dict.update({'Last Access Time': datetime.datetime.fromtimestamp(st.st_atime)})
+        response_dict.update({'Last Modification Time': datetime.datetime.fromtimestamp(st.st_mtime)})
+        response_dict.update({'Last Change Time': datetime.datetime.fromtimestamp(st.st_ctime)})
         response_dict.update({'Index Node': st.st_ino})
         response_dict.update({'Size': st.st_size})
         response_dict.update({'Access Rights': st.st_mode})
